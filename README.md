9d8c6dbaa8202ac2ef33f6b724e1c932ac03cd37
Maliface <MalifaciousGames@proton.me>
2023-12-28 14:31:52 +0100

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Mali-s-Scripts
- Various JS scripts
+## The OmniCoder API
+
+Encode things.
+
+***
+
+## Special passage tags
+
+Enable `script` and `style` passages in Twine 2.
+
+[Special passage tags](special-passage-tags)
+
+***
+
+## Swap markup
+
+Output content at random using this simple markup: `(a|b|c|d)`. 
+
+[Swap markup](swap-markup)
+
+***
+
+## Update markup
+
+Print a variable and automatically update the displayed value when it changes using `{{$myVariable}}`.
+
+[Update markup](update-markup)
+
+***
+
+## URL fixer
+
+Properly localize URLs when using Twine's `Play/Test` feature to launch the game.
+
+[URL fixer](URLfixer)
--- swap-markup/swap-markup.js
+++ swap-markup/swap-markup.js
@@ -1,15 +1,15 @@
 Wikifier.Parser.add({
     name: 'swapMarkup',
     match: '\\(.*?\\|.*?\\)',
-    handler: ({ matchText: m, output: o }) => {
-        let acc = 0, entries = m.substring(1, m.length - 1).split('|').map((e, i) => {
+    handler: ({matchText: m, output: o}) => {
+      	let acc = 0, entries = m.substring(1, m.length - 1).split('|').map((e,i) => {
             let hasOdd = e.match(/^(\d*\.*\d+)(?=:)/), odd = 1, txt = e;
             if (hasOdd) {
                 odd = Number(hasOdd[0]);
-                txt = txt.slice(hasOdd[0].length + 1);
+                txt = txt.slice(hasOdd[0].length+1);
             }
-            odd = acc += odd;
-            return { odd, txt };
+          	odd = acc += odd;
+          	return {odd,txt};
         });
 
         return $(o).wiki(entries.find(e => e.odd > randomFloat(acc)).txt);
