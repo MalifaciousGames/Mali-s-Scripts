818ec878d31deca468439d35be3b05eec40e8b49
Maliface <MalifaciousGames@proton.me>
2025-03-17 14:23:40 +0100

    added easy-keys

---
+++ twine-scripts/easy-keys/easy-keys.js
@@ -0,0 +1,37 @@
+; {
+
+   const doNothing = e =>
+      e.key === 'Enter' ||
+      ['INPUT', 'TEXTAREA'].includes(e.target.nodeName) ||
+      e.target.attributes.hasOwnProperty('contenteditable');
+
+   const buildTargetCode = e => {
+      let targetCode = (e.ctrlKey ? 'ctrl + ' : '') + (e.shiftKey ? 'shift + ' : '');
+
+      if (e.code.startsWith('Digit')) {
+         targetCode += e.code.slice(5);
+      } else if (e.code === 'Space') {
+         targetCode += 'space';
+      } else {
+         targetCode += e.key.toLowerCase();
+      }
+
+      return targetCode;
+   };
+
+   const findTarget = el => el.querySelector('a, button, tw-link, passage-link, [onclick], input, select, textarea') ?? el;
+
+   // bind listener
+   window.addEventListener('keyup', e => {
+
+      if (doNothing(e)) return;
+
+      const targets = document.querySelectorAll(`[data-key="${buildTargetCode(e)}"]`);
+
+      for (let target of targets) {
+         findTarget(target).click();
+      }
+
+   }, true);
+
+};
---
+++ twine-scripts/easy-keys/easy-keys.md
@@ -0,0 +1,25 @@
+## Easy keys
+
+This script allows users to bind key combinations to DOM elements by using the `data-key` attribute.
+
+**Syntax**
+```html
+On a clickable element:
+<a data-key="g" onclick="console.log('Something.')">Do something (g)</a>
+
+On a wrapper element:
+<span data-key="shift + b">
+   <button onclick="console.log('Something else.')">Do something else (shift + b)</button>
+</span>
+
+```
+
+### Key expressions
+
+Key expressions are **always written in lowercase**. They accept `ctrl + ` and `shift + ` as prefixes.
+
+- digit keys and numpad keys output their associated number
+- the space bar outputs `space`
+- the control key alone outputs `control`
+
+Do keep in mind that some keys combinations are browser or system shortcuts and cannot be used as bindings, please do your research!
